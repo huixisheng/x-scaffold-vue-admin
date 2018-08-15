912afb43198d8664b78150d6373783561d4e9fc2
huixisheng <469184346@qq.com>
2018-08-15 22:51:52 +0800

    feat: support multipage

--- template/vue.config.js
+++ template/vue.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const AutoDllPlugin = require('autodll-webpack-plugin');
 
-
 const devServer = require('./config/webpack-dev-server');
+const entryMultupage = require('./config/entry-multipage');
 const qiniuWebpackPlugin = require('./config/qiniu-plugin');
 const { getEnvConfig, publicPath } = require('./config/utils');
 const webpackAssetsManifessInstance = require('./config/deploy-manifest');
@@ -19,6 +19,8 @@ function resolve(dir) {
 // }
 
 module.exports = {
+  pages: entryMultupage,
+
   lintOnSave: true,
   // https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
   baseUrl: process.env.NODE_ENV === 'production' ? publicPath : '/',
