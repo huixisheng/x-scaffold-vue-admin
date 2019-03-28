58a02a56842bdd4950e88941570c2b34bad8ab37
huixisheng <469184346@qq.com>
2019-03-28 09:30:43 +0800

    feat: custom store

--- template/src/layouts/vue-element-admin/store/index.js
+++ template/src/layouts/vue-element-admin/store/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import app from './modules/app'
 import errorLog from './modules/errorLog'
-import permission from './modules/permission'
+// 修改为自定义
+import permission from 'src/store/modules/permission'
 import tagsView from './modules/tagsView'
 import settings from './modules/settings'
-import user from './modules/user'
+// 修改为自定义
+import user from 'src/store/modules/user'
 import getters from './getters'
 
 Vue.use(Vuex)
